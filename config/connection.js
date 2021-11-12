debd31c1d8f267d2540e14a34d0bd81ff114c0c5
Andres Pena <penchjpc@gmail.com>
2021-11-12 09:47:59 -0500

    defined sequelize models, defined environment variables and server.js sync sequelize models to the database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
@@ -12,4 +11,5 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
-module.exports = sequelize;
+    module.exports = sequelize;
+
