bc544a09294b312e75a8fd84c3b47a28575a2874
Andres Pena <penchjpc@gmail.com>
2021-11-13 11:13:19 -0500

    successfully tested POST route to create tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,7 +30,35 @@ router.get('/:id', async (req, res) => {
 });
 // create a new tag
 router.post('/', (req, res) => {
-  
+/* req.body should look like this...
+    {
+      tag_name: "Camping",
+      productIds: [1, 2, 3, 4]
+    }
+  */
+//  first create new tag
+  Tag.create(req.body)
+  .then((newTag) => {
+    //then, if there are product ids in the productIds array, create tag_id pairings to populate the ProductTag model
+    if(req.body.productIds.length){
+      // create new array of objects witn product_id and tag_id pairings, where the product_id is obtained from the productIds array passed in the request body
+      let productTagIdArr = req.body.productIds.map((productId) =>{
+        return {
+          product_id: productId,
+          tag_id: newTag.id,
+        }
+      })
+      // populate ProductTag model
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // If no productIds array, then just send the object of the created tag
+    res.status(200).json(newTag);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds) )
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 // update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
